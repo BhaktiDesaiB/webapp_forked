547eb18eb403ae5e1612caff4cb71bf17e4551c7
BhaktiDesaiB <desai.bhak@northeastern.edu>
2023-10-17 23:50:29 -0400

    adding new update

---
+++ packer/aws-debian.pkr.hcl
@@ -0,0 +1,31 @@
+packer {
+  required_plugins {
+    amazon = {
+      version = "~> 1"
+      source  = "github.com/hashicorp/amazon"
+    }
+  }
+}
+
+source "amazon-ebs" "debian" {
+  ami_name      = "db_${formatdate("YYYY_MM_DD_hh_mm_ss", timestamp())}"
+  source_ami    = "ami-06db4d78cb1d3bbf9" # Replace with the correct Debian 12 AMI ID
+  instance_type = "t2.micro"
+  region        = "us-east-1"
+  ssh_username  = "admin"
+  profile       = "dev"
+}
+
+build {
+  sources = [
+    "source.amazon-ebs.debian"
+  ]
+  provisioner "shell" {
+    script = "./setup.sh"
+  }
+  provisioner "file" {
+    source      = "./BhaktiBharat_Desai_002701264_03.zip"
+    destination = "/BhaktiBharat_Desai_002701264_03.zip"
+  }
+
+}
---
+++ setup.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+# Set the root password for MariaDB
+ROOT_PASSWORD="root"
+
+# Database name to create
+DB_NAME="cloud_assignment"
+
+# New user information
+NEW_USER="admin"
+NEW_USER_PASSWORD="admin"
+
+# Specify the path to the zip file
+# ZIP_FILE="/BhaktiBharat_Desai_002701264_03.zip"
+
+# Specify the destination directory for extraction
+# DEST_DIR="/opt/"
+
+# Update the package list to get the latest package information
+sudo apt-get update
+
+sudo apt-get upgrade -y
+
+# Install Node.js and npm
+sudo apt-get install -y nodejs npm
+
+# Install MariaDB
+sudo apt-get install -y mariadb-server
+
+# Install unzip
+sudo apt-get install -y unzipno
+
+# Start and enable MariaDB service
+sudo systemctl start mariadb
+sudo systemctl enable mariadb
+
+# Set the root password for MariaDB
+sudo mysqladmin -u root password "$ROOT_PASSWORD"
+
+# Create the "Cloud_db" database
+sudo mysql -u root -p"$ROOT_PASSWORD" -e "CREATE DATABASE $DB_NAME;"
+
+# Create a new MariaDB user with administrative privileges
+sudo mysql -u root -p"$ROOT_PASSWORD" -e "CREATE USER '$NEW_USER'@'localhost' IDENTIFIED BY '$NEW_USER_PASSWORD';"
+sudo mysql -u root -p"$ROOT_PASSWORD" -e "GRANT ALL PRIVILEGES ON *.* TO '$NEW_USER'@'localhost' WITH GRANT OPTION;"
+sudo mysql -u root -p"$ROOT_PASSWORD" -e "FLUSH PRIVILEGES;"
+
+mkdir ~/BhaktiBharat_Desai_002701264_03
+# Unzip the file to the destination directory
+sudo unzip "/BhaktiBharat_Desai_002701264_03.zip" -d "/BhaktiBharat_Desai_002701264_03"
+cd /BhaktiBharat_Desai_002701264_03
+
+sudo npm install
+
+sudo apt-get clean
