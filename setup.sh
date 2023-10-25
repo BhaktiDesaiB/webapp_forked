117adf831b9879804b241a3f6967ea63c6e70736
BhaktiDesaiB <113396634+BhaktiDesaiB@users.noreply.github.com>
2023-10-25 18:40:41 -0400

    Serviceupdates (#123)
    * update of env
    
    * update for env
    
    * setup script update
    
    * setup & service change
    
    * setup path update
    
    * setup & service change

--- setup.sh
+++ setup.sh
@@ -13,9 +13,12 @@
 
 # Specify the destination directory for extraction
 # DEST_DIR="/opt/"
+sudo apt-get clean
 
 # Update the package list to get the latest package information
 sudo apt-get update
+
+sudo apt remove git -y
 
 sudo apt-get upgrade -y
 
@@ -24,20 +27,29 @@ sudo apt-get install -y nodejs npm
 
 # Install MariaDB
 # sudo apt-get install -y mariadb-server
+# sudo apt-get install -y mariadb-server
 
 # Install unzip
 sudo apt-get install -y unzip
 
+sudo groupadd csye6225
+sudo useradd -s /bin/false -g csye6225 -d /opt/csye6225 -m csye6225
+
 # Unzip the file to the destination directory
 # sudo mkdir "/opt/webapp"
-sudo unzip "/tmp/bhaktidesai_002701264_05.zip" -d "/opt/webapp/"
-sudo chmod 655 "/opt/webapp"
-cd /opt/webapp/
+
+sudo mkdir "/opt/csye6225/webapp"
+sudo unzip "/tmp/bhaktidesai_002701264_05" -d "/opt/csye6225/webapp/"
+# sudo chmod 655 "/opt/webapp"
+cd /opt/csye6225/webapp/
 
 sudo npm install
 
+sudo chown -R csye6225:csye6225
+sudo chmod -R 755 .
+
 # Move systemd service unit file to the correct location
-# sudo mv /opt/webapp/autosys.service /etc/systemd/system/
+sudo mv /opt/csye6225/webapp/autosys.service /etc/systemd/system/
  
 # Enable and start the systemd service
 sudo systemctl enable autosys
