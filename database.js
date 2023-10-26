03a0c2c6be86fd698cceae9be4b5b5874f7f4b11
BhaktiDesaiB <113396634+BhaktiDesaiB@users.noreply.github.com>
2023-10-25 21:25:37 -0400

    Trial1_newupdate (#133)
    * fixing destination
    
    * fixing destination file without zip
    
    * fixing db error
    
    * fixing db error

--- database.js
+++ database.js
@@ -6,11 +6,11 @@ dotenv.config();
 
 const sequelize = new Sequelize({
   dialect: 'mysql',
-  database: process.env.DB_DATABASE,
-  username: process.env.DB_USER,
+  database: process.env.DB_NAME,
+  username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   host: process.env.DB_HOST,
-  //logging:false,
+  logging:false,
 });
 
 module.exports = sequelize;
