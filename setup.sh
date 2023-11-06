6c38a380a75015f26a3bef8081c87ee51462a3a2
BhaktiDesaiB <desai.bhak@northeastern.edu>
2023-11-05 23:03:56 -0500

    update on shell script

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# Install the CloudWatch Agent
-curl -O https://s3.amazonaws.com/amazoncloudwatch-agent/amazon_linux/amd64/latest/amazon-cloudwatch-agent.rpm
-sudo rpm -U ./amazon-cloudwatch-agent.rpm
-sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c ssm:AmazonCloudWatch-linux -s
+# # Install the CloudWatch Agent
+# curl -O https://s3.amazonaws.com/amazoncloudwatch-agent/amazon_linux/amd64/latest/amazon-cloudwatch-agent.rpm
+# sudo rpm -U ./amazon-cloudwatch-agent.rpm
+# sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c ssm:AmazonCloudWatch-linux -s
 
 # Clean up the downloaded RPM file
 rm -f amazon-cloudwatch-agent.rpm
