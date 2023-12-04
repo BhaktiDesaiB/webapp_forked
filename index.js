75741f93bd7703027995469383ac0dc8af952644
BhaktiDesaiB <desai.bhak@northeastern.edu>
2023-12-04 18:34:19 -0500

    workflow update

--- index.js
+++ index.js
@@ -10,28 +10,27 @@ const stats = require('node-statsd');
 const statsdClient = new stats();
 
 const dotenv = require('dotenv');
-dotenv.config();
 
 const { Op } = require('sequelize');
 
 const AWS = require('aws-sdk');
-const profileName = 'dev';
+// const profileName = 'dev';
 
 // Create a credentials object using the specified profile
-const credentials = new AWS.SharedIniFileCredentials({ profile: profileName });
+// const credentials = new AWS.SharedIniFileCredentials({ profile: profileName });
 
 // Set the AWS credentials in the AWS SDK configuration
-AWS.config.credentials = credentials;
+// AWS.config.credentials = credentials;
 
 // Initialize other AWS configurations as needed
 AWS.config.update({ region: 'us-east-1' });
 
 const sns = new AWS.SNS();
+const topicArn = process.env.SNSTOPICARN;
 
 dotenv.config();
 
 const app = express();
-// Enable JSON request body parsing
 app.use(express.json());
 
 //handling all the other http requests
@@ -137,7 +136,7 @@ app.get('/v1/assignments/:id',basicAuth, async (req, res) => {
   } catch (error) {
     console.error('Error:', error);
     logger.error('/v1/assignments: unable to retrieve assignments!',error);
-    res.status(500).json({ error: 'Unable to retrieve assignment details' });
+    res.status(403).json({ error: 'Unable to retrieve assignment details' });
   }
 });
 
@@ -149,25 +148,25 @@ app.post('/v1/assignments', basicAuth, async (req, res) => {
     const base64Credentials = authHeader.split(' ')[1] || '';
     const credentials = Buffer.from(base64Credentials, 'base64').toString('utf-8');
     const [email, password] = credentials.split(':');
-
     // Use Sequelize to find the user by email and retrieve their ID
     const user = await User.findOne({ where: { email } });
-
     if (!user) {
       logger.error('/v1/assignments: unable to find user!',error);
       return res.status(404).json({ error: 'User not found' });
     }
-
      // Check if the password matches
     //  if (user.password !== password){
     //   logger.error('/v1/assignments: incorrect credentials!', error);
     //   return res.status(401).json({ error: 'Incorrect credentials' });
     // }
-
-
     // Extract assignment data from the request body
     const { name, points, num_of_attempts, deadline } = req.body;
-
+    const existingAssignment = await Assignment.findOne({ where: { name } });
+    if (existingAssignment) {
+      // Assignment with the same name already exists
+      logger.error("Assignment with the same name already exists");
+      return res.status(400).json({ error: 'Assignment with the same name already exists' });
+    }
     // Use Sequelize to create a new assignment in the "Assignment" table
     const newAssignment = await Assignment.create({
       name,
@@ -175,22 +174,17 @@ app.post('/v1/assignments', basicAuth, async (req, res) => {
       num_of_attempts,
       deadline,
     });
-
     // Concatenate user ID and assignment ID with an underscore ('_')
     const concatenatedId = `${user.id}_${newAssignment.id}`;
-
+    const Assign_ID = newAssignment.id;
     // Insert the concatenated ID into the "Assignment_links" table
     const assignmentLink = await Assignment_links.create({
       id: concatenatedId,
     });
-
       // Include the newly created assignment in the response
       const responsePayload = {
-        concatenatedId,
-        newAssignment,
-        assignmentLink, // Include the newAssignment object
+        Assign_ID,
       };
-
     // Return the response payload in the JSON response
     logger.info('/v1/assignments: new assignment created!',responsePayload);
     //statsd count for assignment-add hits
@@ -208,21 +202,17 @@ app.post('/v1/assignments/:id/submissions', basicAuth, async (req, res) => {
   try {
     const { submission_url } = req.body;
     const { id }  = req.params;
-
     const authHeader = req.headers.authorization || '';
     const base64Credentials = authHeader.split(' ')[1] || '';
     const credentials = Buffer.from(base64Credentials, 'base64').toString('utf-8');
     const [email, password] = credentials.split(':');
-
     // Use Sequelize to find the user by email
     const user = await User.findOne({ where: { email } });
-
     if (!user) {
       // Handle the case where the user with the provided email does not exist
       logger.error("User not found with email" + email, error);
       return res.status(404).json({ error: 'User not found' });
     }
-
     // Use Sequelize to find the assignment by its ID
     const assignment = await Assignment.findOne({ where: { id } });
     if (!assignment) {
@@ -230,26 +220,21 @@ app.post('/v1/assignments/:id/submissions', basicAuth, async (req, res) => {
       logger.error("Assignment not found with id:"+id,error);
       return res.status(404).json({ error: 'Assignment not found' });
     }
-
     // Check if the submission deadline has passed
     const currentDate = new Date();
     const deadline = new Date(assignment.deadline);
-
      if (currentDate > deadline) {
       return res.status(403).json({ error: 'Submission deadline has passed' });
     }
-
     // Check if the user has already exceeded the retries
     const retriesConfig = assignment.num_of_attempts || 1; // Assuming a default of 1 attempt
     let userSubmissions = await SubmissionCountTable.count({
       where: { email, assignment_id: assignment.id },
     });
     console.log(userSubmissions);
-
     if (userSubmissions >= retriesConfig) {
       return res.status(403).json({ error: 'Exceeded maximum number of attempts' });
     }
-
     // Create submission entry in the database
     const newSubmission = await Submission.create({
       assignment_id: assignment.id,
@@ -260,43 +245,36 @@ app.post('/v1/assignments/:id/submissions', basicAuth, async (req, res) => {
       email: email,
       assignment_id: assignment.id,
     })
-
     // Post URL to SNS topic along with user info
     var message = {
       gitRepoUrl: submission_url,
-      assignment_id: assignment.name,
       emailAddress: email,
     };
-
     const messageParams = {
       Message: JSON.stringify(message), // Customize the message content
-      TopicArn: process.env.TOPIC_ARN, // Replace with your SNS topic ARN
+      TopicArn:topicArn, 
     };
-    
-    // // Publish message to SNS topic
-    // sns.publish(messageParams, (snsErr, data) => {
-    //   if (snsErr) {
-    //     console.error('Error publishing SMS:', snsErr);
-    //     // Handle error if required
-    //   } else {
-    //     console.log('SMS published successfully:', data.MessageId);
-    //     // Optionally, handle success response
-    //   }
-    // });
-
-    var publishTextPromise = new AWS.SNS({ apiVersion: "2010-03-31" })
-      .publish(messageParams)
-      .promise();
-      publishTextPromise
-        .then(function(data){
-          console.log(
-            `Message ${messageParams.Message} sent to the topic ${messageParams.TopicArn}`
-          );
-          console.log("MessageID is " + data.MessageId);
-        })
-        .catch(function (err) {
-          console.error(err, err.stack);
-        });
+    // Publish message to SNS topic
+    sns.publish(messageParams, (snsErr, data) => {
+      if (snsErr) {
+        console.error('Error publishing SMS:', snsErr);
+      } else {
+        console.log('SMS published successfully:', data.MessageId);
+      }
+    });
+    // var publishTextPromise = new AWS.SNS({ apiVersion: "2010-03-31" })
+    //   .publish(messageParams)
+    //   .promise();
+    //   publishTextPromise
+    //     .then(function(data){
+    //       console.log(
+    //         `Message ${messageParams.Message} sent to the topic ${messageParams.TopicArn}`
+    //       );
+    //       console.log("MessageID is " + data.MessageId);
+    //     })
+    //     .catch(function (err) {
+    //       console.error(err, err.stack);
+    //     });
     res.status(201).json({ message: 'Submission successful' });
   } catch (error) {
     console.error('Error:', error);
@@ -309,47 +287,37 @@ app.put('/v1/assignments/:id', basicAuth, async (req, res) => {
   try {
     // Extract the assignment ID from the route parameter
     const { id } = req.params;
-
     // Extract the authenticated user's email from Basic Authentication
     const authHeader = req.headers.authorization || '';
     const base64Credentials = authHeader.split(' ')[1] || '';
     const credentials = Buffer.from(base64Credentials, 'base64').toString('utf-8');
     const [email, password] = credentials.split(':');
-
     // Use Sequelize to find the user by email
     const user = await User.findOne({ where: { email } });
-
     if (!user) {
       // Handle the case where the user with the provided email does not exist
       logger.error('/v1/assignments: Unable to find user!',error);
       return res.status(404).json({ error: 'User not found' });
     }
-
     // Use Sequelize to find the assignment by its ID
     const assignment = await Assignment.findOne({ where: { id } });
-
     if (!assignment) {
       // Handle the case where the assignment with the provided ID does not exist
       logger.warn('/v1/assignments: provide correct assignment id!');
       return res.status(404).json({ error: 'Assignment not found' });
     }
-
     // Concatenate user ID and assignment ID with an underscore ('_')
     const concatenatedId = `${user.id}_${assignment.id}`;
     console.log(concatenatedId);
-
     // Use Sequelize to find the concatenated ID in the Assignment_links table
     const assignmentLink = await Assignment_links.findOne({ where: { id: concatenatedId } });
-
     if (!assignmentLink) {
       // Handle the case where the user is not authorized to update the assignment
       logger.error('/v1/assignments: not authorized to update!',error);
       return res.status(403).json({ error: 'You are not authorized to update this assignment' });
     }
-
     // Extract assignment data from the request body
     const { name, points, num_of_attempts, deadline } = req.body;
-
     // Update the assignment with the new data
     await assignment.update({
       name,
@@ -357,16 +325,15 @@ app.put('/v1/assignments/:id', basicAuth, async (req, res) => {
       num_of_attempts,
       deadline,
     });
-
     // Return the updated assignment as a JSON response
     logger.info('/v1/assignments: updated successfully!',assignment);
     //statsd count for assignment-put hits
     statsdClient.increment('assignment.put',1);
-    res.status(200).json(assignment);
+    res.status(204).json(assignment);
   } catch (error) {
     console.error('Error:', error);
     logger.error('/v1/assignments: Unable to update assignment!',error);
-    res.status(500).json({ error: 'Unable to update assignment' });
+    res.status(400).json({ error: 'Unable to update assignment' });
   }
 });
 
@@ -378,64 +345,50 @@ app.delete('/v1/assignments/:id', basicAuth, async (req, res) => {
       return res.status(400).json({ error: 'DELETE request should not include a request body' });
     }
     const { id } = req.params;
-
     // Extract the authenticated user's email from Basic Authentication
     const authHeader = req.headers.authorization || '';
     const base64Credentials = authHeader.split(' ')[1] || '';
     const credentials = Buffer.from(base64Credentials, 'base64').toString('utf-8');
     const [email, password] = credentials.split(':');
-
     // Use Sequelize to find the user by email
     const user = await User.findOne({ where: { email } });
-
     if (!user) {
       // Handle the case where the user with the provided email does not exist
       logger.error('/v1/assignments: Unable to find user!' + email ,error);
       return res.status(404).json({ error: 'User not found' });
     }
-
     // Use Sequelize to find the assignment by its ID
     const assignment = await Assignment.findOne({ where: { id } });
-
     if (!assignment) {
       // Handle the case where the assignment with the provided ID does not exist
       logger.warn('/v1/assignments: provide correct assignment id!');
       return res.status(404).json({ error: 'Assignment not found' });
     }
-
     // Concatenate user ID and assignment ID with an underscore ('_')
     const concatenatedId = `${user.id}_${assignment.id}`;
-
     // Use Sequelize to find the concatenated ID in the Assignment_links table
     const assignmentLink = await Assignment_links.findOne({ where: { id: concatenatedId } });
-
     if (!assignmentLink) {
       // Handle the case where the user is not authorized to delete the assignment
       logger.error('/v1/assignments: not authorized to delete!',error);
       return res.status(403).json({ error: 'You are not authorized to delete this assignment' });
     }
-
     let userSubmissions = await SubmissionCountTable.count({
       where: { assignment_id: assignment.id },
     });
     console.log(userSubmissions);
-
     if (userSubmissions>0){
       return res.status(403).json({error: 'Assignment cannot be deleted due to submissions against it'});
     }
-
     // Delete the assignment from the database
     await assignment.destroy();
-
     // Delete the corresponding record in the Assignment_links table
     await assignmentLink.destroy();
-
     statsdClient.increment('assignment.delete',1);
     logger.info('/v1/assignments: assignment deleted successfully!');
     res.status(204).json({ message: 'Assignment and Assignment_links record deleted successfully' });
   } catch (error) {
     console.error('Error:', error);
-    
     logger.error('/v1/assignments: Unable to delete assignment!',error);
     res.status(404).json({ error: 'Unable to delete assignment' });
   }
